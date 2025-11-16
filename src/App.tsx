122b9b2279e148b396d7586f3a295e86e130b22a
Bikram Rai <bikramraidev@gmail.com>
2025-11-16 09:32:59 +0545

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex text-4xl justify-center text-center items-center text-amber-300">
+        kanban board
+      </div>
+    </>
+  );
+}
+
+export default App;
