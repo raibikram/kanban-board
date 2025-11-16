555b8016a655d85b0d53adff24e3f9ae064246f3
Bikram Rai <bikramraidev@gmail.com>
2025-11-16 09:42:04 +0545

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+
 function App() {
   return (
     <>
+      <Header />
       <div className="h-screen flex text-4xl justify-center text-center items-center text-amber-300">
         kanban board
       </div>
+      <Footer />
     </>
   );
 }
