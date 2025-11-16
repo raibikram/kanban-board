fb2c9fdf91e8b4d72781917a83027c6a8ea2851d
Bikram Rai <bikramraidev@gmail.com>
2025-11-16 17:28:39 +0545

    improve header ui

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,21 @@
-
+import { motion } from "framer-motion";
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <motion.header
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ type: "spring", stiffness: 120, damping: 20 }}
+      whileHover={{ scale: 1.03 }}
+      className="top-8 transform px-8 py-4"
+    >
+      <div className="flex flex-col items-center gap-1  rounded-xl px-6 py-4">
+        <h1 className="text-2xl md:text-3xl font-bold text-rose-600 dark:text-yellow-300 tracking-wide">
+          Kanban Board
+        </h1>
+        <p className="text-sm md:text-base text-gray-700 dark:text-red-300 italic">
+          Minimal, Calm & Cozy
+        </p>
+      </div>
+    </motion.header>
+  );
 }
